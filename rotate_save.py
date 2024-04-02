27d4132348869bde2fc7df754c40603b5ecf10e9
SumDeusVitae <kyrylosorokovskyi@gmail.com>
2024-04-01 22:12:14 -0500

    removed pics

---
+++ rotate_save.py
@@ -0,0 +1,34 @@
+from PIL import Image 
+import os
+import argparse
+import time
+from datetime import timedelta
+
+def rotate_save(adress: str, file: str) -> None:
+    format = file[-3:]
+    name = file[:-4]
+    Original_Image = Image.open(fr'{adress}\{file}') 
+    rotated_image1 = Original_Image.rotate(180)
+    rotated_image1 = rotated_image1.save(fr'{adress}\{name}_180.{format}')
+    rotated_image2 = Original_Image.transpose(Image.ROTATE_90) 
+    rotated_image2 = rotated_image2.save(fr'{adress}\{name}_90.{format}')
+    rotated_image3 = Original_Image.rotate(60)
+    rotated_image3 = rotated_image3.save(fr'{adress}\{name}_60.{format}')
+
+def main(path: str) -> None:
+    start_time = time.monotonic()
+    dir_list = os.listdir(path)
+    for im in dir_list:
+        rotate_save(adress=path, file=im)
+        # print(path)
+    print(f'Worked on {len(dir_list)} pictures')
+    end_time = time.monotonic()
+    print(f'Took {round(end_time - start_time, 4)} seconds to complete')
+   
+
+
+parser = argparse.ArgumentParser(description = 'rotates and saves images')
+parser.add_argument('path', metavar = 'path', type=str, help='*.data path')
+args = parser.parse_args()
+path = args.path
+main(path)
---
+++ screenshot.py
@@ -0,0 +1,52 @@
+import pyautogui
+import time
+import keyboard
+import win32api
+import os
+
+win32api.MessageBox(0, 'Press "s" to take screenshot, "d" to delete all, "q" to exit', 'Instruction')
+
+def active_window_screenshot(active_window: object) -> None:
+    # active_window.topleft = (0,0) // moves window to the left corner
+    active_window_region = (active_window.left, active_window.top, active_window.right, active_window.bottom)
+    active_window.activate()
+    t = time.localtime()
+    pyautogui.screenshot(fr'images/screenshot{t.tm_hour}{t.tm_min}{t.tm_sec}.png',region = active_window_region)
+
+
+def main() -> None:
+    count = 0
+    while(True):
+        if keyboard.is_pressed('s'):
+            count+=1
+            print(f'Took screenshot {count} {'time' if count == 1 else 'times'}')
+            active = pyautogui.getActiveWindow()
+            active_window_screenshot(active_window=active)
+            time.sleep(1)
+        if keyboard.is_pressed('d'):
+            cur_time = time.localtime()
+            win32api.MessageBox(0, 'Press "y" to delete all or "n" to terminate', 'Notification')
+            while(True):
+                if keyboard.is_pressed('y'):
+                    dir_list = os.listdir('./images')
+                    for each in dir_list:
+                        os.remove(fr'./images/{each}')
+                    win32api.MessageBox(0, 'Deleted all screenshots', 'Notification')    
+                    break    
+                if keyboard.is_pressed('n'): 
+                    break       
+                if ((time.localtime().tm_sec - cur_time.tm_sec) > 5):
+                    print('Terminated task due to inactivity')
+                    break
+        if keyboard.is_pressed('q'):
+            win32api.MessageBox(0, 'Application Closed', 'Notification')
+            print('Application terminated')
+            return
+        
+
+        continue
+
+main()
+
+
+
