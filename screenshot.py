dc5b74f1058c2311c311e1d076cc5cb05388baaf
SumDeusVitae <kyrylosorokovskyi@gmail.com>
2024-04-02 11:08:36 -0500

    finally

--- screenshot.py
+++ screenshot.py
@@ -3,25 +3,19 @@ import time
 import keyboard
 import win32api
 import os
+from PIL import ImageGrab
 
-win32api.MessageBox(0, 'Press "s" to take screenshot, "d" to delete all, "q" to exit', 'Instruction')
+win32api.MessageBox(0, 'Press "ctrl+s" to take screenshot, "ctrl+q" to exit', 'Instruction')
 
-def active_window_screenshot(active_window: object) -> None:
-    # active_window.topleft = (0,0) // moves window to the left corner
-    active_window_region = (active_window.left, active_window.top, active_window.right, active_window.bottom)
-    active_window.activate()
-    t = time.localtime()
-    pyautogui.screenshot(fr'images/screenshot{t.tm_hour}{t.tm_min}{t.tm_sec}.png',region = active_window_region)
-
-
+"""
 def main() -> None:
-    count = 0
+    count: int = 0
     while(True):
         if keyboard.is_pressed('s'):
             count+=1
             print(f'Took screenshot {count} {'time' if count == 1 else 'times'}')
-            active = pyautogui.getActiveWindow()
-            active_window_screenshot(active_window=active)
+            
+            active_window_screenshot()
             time.sleep(1)
         if keyboard.is_pressed('d'):
             cur_time = time.localtime()
@@ -45,8 +39,66 @@ def main() -> None:
         
 
         continue
+"""
 
-main()
+def timer(amount: int) -> None:
+     while amount > 0:
+          print(f'Task will be performed in {amount} {"second" if amount == 1  else "seconds"}')
+          time.sleep(1)
+          amount-= 1
+"""
+     
+         
+
+def delete_all():
+    win32api.MessageBox(0, 'Press "y" to delete all or "n" to terminate', 'Confirmation')
+    keyboard.add_hotkey('ctrl+y', delete)
+    keyboard.add_hotkey('ctrl+n', quit)
+    timer(4)
+    # keyboard.remove_hotkey('ctrl+y')
+    # keyboard.remove_hotkey('ctrl+n')
+"""    
+
+def active_window_screenshot() -> None:
+    # active_window.topleft = (0,0) // moves window to the left corner
+    active: object = pyautogui.getActiveWindow()
+    t: object = time.localtime()
+    # Getting active region and adjusting it
+    active_window_region = (active.left+7, active.top, active.right-7, active.bottom-7)
+    print(f'Window: {active.title}, Bbox: {active_window_region}')
+    screenshot: object = ImageGrab.grab(bbox=active_window_region)
+    # screen = pyautogui.screenshot(region = active_window_region)
+    path_name: str = fr'images/screenshot{t.tm_hour}{t.tm_min}{t.tm_sec}.png'
+    # Saving screenshot
+    try:
+        screenshot.save(path_name)
+    except FileNotFoundError:
+        os.mkdir('images')
+        print('Created directory images')
+        screenshot.save(path_name)
+
+"""
+def delete():
+        print('delete pressed')
+        dir_list = os.listdir('./images')
+        for each in dir_list:
+            os.remove(fr'./images/{each}')
+        win32api.MessageBox(0, 'Deleted all screenshots', 'Notification')
+"""
+def exiting() -> None:
+    print('Exit was pressed')
+    timer(3)
+    print('Bye-Bye')
+    os._exit(0)
+
+def main() -> None:
+    keyboard.add_hotkey('ctrl+s', active_window_screenshot)
+    # keyboard.add_hotkey('ctrl+d', delete)
+    keyboard.add_hotkey('ctrl+q', exiting)
+    keyboard.wait()
+
+if __name__ == "__main__":
+    main()
 
 
 
